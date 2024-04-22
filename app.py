efc509eaf7ce4a32345a2442d513e51fac0316ef
Rogerio Ricardo <rrssouza@yahoo.com.br>
2024-04-22 17:52:42 -0300

    criado layout dash plotly

---
+++ app.py
@@ -0,0 +1,13 @@
+from dash import Dash
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as ps
+import numpy as np
+
+app = Dash(__name__)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
